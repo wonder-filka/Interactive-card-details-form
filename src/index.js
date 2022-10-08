9d799a5a69a9670a9a063e61384dfb619f6df862
Ira Filonova <74858792+wonder-filka@users.noreply.github.com>
2022-10-08 15:54:11 +0300

    -

--- src/index.js
+++ src/index.js
@@ -29,30 +29,6 @@ userCvc.addEventListener("input", updateCvc);
 
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  form.innerHTML = `
-    <div class="complete text-center">
-                <div class="imgComplete m-4">
-                    <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="40" cy="40" r="40" fill="url(#a)" />
-                        <path d="M28 39.92 36.08 48l16-16" stroke="#fff" stroke-width="3" />
-                        <defs>
-                            <linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507"
-                                gradientUnits="userSpaceOnUse">
-                                <stop stop-color="#6348FE" />
-                                <stop offset="1" stop-color="#610595" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                </div>
-                <h1>THANK YOU!</h1>
-                <p id="parConfirm">We've added your card details</p>
-                <button type="button" value="Continue" class="button w-75 m-4">Continue</button>
-            </div>
-    `;
-}
-
 function updateNumber(e) {
   let numerValue = e.target.value;
   if (isNaN(numerValue)) {
@@ -129,4 +105,28 @@ function updateCvc(e) {
       document.getElementById("userCvc").className = "elseBlock inputWar";
     }
   }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  form.innerHTML = `
+    <div class="complete text-center">
+                <div class="imgComplete m-4">
+                    <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="40" cy="40" r="40" fill="url(#a)" />
+                        <path d="M28 39.92 36.08 48l16-16" stroke="#fff" stroke-width="3" />
+                        <defs>
+                            <linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507"
+                                gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#6348FE" />
+                                <stop offset="1" stop-color="#610595" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </div>
+                <h1>THANK YOU!</h1>
+                <p id="parConfirm">We've added your card details</p>
+                <button type="button" value="Continue" class="button w-75 m-4">Continue</button>
+            </div>
+    `;
 }
